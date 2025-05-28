288d902dc215219d455640c64c68213949b75b61
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-05-28 19:34:56 +0600

    images added & header img implemented

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+   <div className="container bg-[url('/images/pattern-bg-desktop.png')] h-80vh w-screen">
+     <h1>IP Address Tracker</h1>
+     <div>
+      <input type="text" />
+      <button></button>
+     </div>
+   </div>
+    </>
+  )
+}
+
+export default Header
