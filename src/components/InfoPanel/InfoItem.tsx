939e2bf82ce688f8dae8f1cffae741d818b96040
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-06-11 19:50:57 +0600

    static infoItem added

--- src/components/InfoPanel/InfoItem.tsx
+++ src/components/InfoPanel/InfoItem.tsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const InfoItem = () => {
   return (
-    <div>InfoItem</div>
+    <div className='flex items-center justify-around'>
+        <div className='border-r-1 flex flex-col justify-center pr-[10%]'>
+            <h3>ip <address></address></h3>
+        <p>192.212.174.101</p>
+        </div>
+        <div className='border-r-1 pr-[15%]'>
+            <h3>location</h3>
+            <p>Brooklyn, NY 10001</p>
+        </div>
+        <div className='border-r-1 pr-[10%]'>
+            <h3>timezone</h3>
+            <p>utc -05:00</p>
+        </div>
+        <div className=''>
+            <h3>isp</h3>
+            <p>SpaceX<br/>
+                Starlink
+            </p>
+        </div>
+    </div>
   )
 }
 
--- src/components/InfoPanel/InfoPanel.tsx
+++ src/components/InfoPanel/InfoPanel.tsx
@@ -4,7 +4,7 @@ import InfoItem from './InfoItem'
 function InfoPanel() {
   return (
     <>
-    <div className="w-[80%] h-auto px-2 py-8 bg-white top-50 absolute left-1/2 transform -translate-x-1/2 border-2 ">
+    <div className="w-[80%] h-auto px-2 py-8 bg-white top-50 absolute left-1/2 transform -translate-x-1/2 border-2">
 <InfoItem></InfoItem>
     </div>
     </>
