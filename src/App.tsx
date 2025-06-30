be24f6f4c45be1c2d79396ba07784c425998caa7
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-06-30 19:44:59 +0600

    map init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import InfoPanel from './components/InfoPanel';
+import Map from './components/Map';
 
 const App = () => {
   return (
     <>
     <Header></Header>
     <InfoPanel></InfoPanel>
+    <Map></Map>
     </>
   )
 }
---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Map() {
+  return (
+     <div id="map" className='h-screen w-full'></div>
+  )
+}
+
+export default Map
